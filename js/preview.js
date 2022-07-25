1b5c02b850243d69291ad5ab98bf3c02f0a2e7fd
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-07-24 20:51:40 -0700

    Loops and Arrays
    Did some practice with loops on udemy and then did some practice with arrays with leon

--- js/preview.js
+++ js/preview.js
@@ -196,7 +196,7 @@
 
 //Bonus can you make it print '21' 21 times to the dom?
 
-// ------------------ Practice For Loops -----------------
+// ------------------ Practice For Loops (Udemy)-----------------
 
 // console log even numbers 0 - 20 
 
@@ -222,3 +222,93 @@
 // for(i=0; i<=people.length; i++){
 //   console.log(people[i].toUpperCase())
 // }
+
+
+
+// ------------------ Practice Class 19-----------------
+
+// ------------------ Bring it all or nothin -----------------
+// *Variables*
+// Declare a variable, assign it a boolean, and alert the value
+  // let yesOrNo = "True";
+  // console.log(yesOrNo);
+
+// Declare a variable, reassign it to your favorite color, and console log the value
+  // let favColor = "blue";
+  // console.log(favColor);
+
+// *Functions*
+// Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
+  // function takesFour(num1, num2, num3, num4) {
+  //   return (num1 + num2 + num3) / num4;
+  // }
+
+  // console.log( takesFour(10, 10, 10, 3) );
+  
+
+// Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
+//  function takesTwo(number1 , number2) {
+//     console.log(Math.pow(number1, number2));
+//  }
+
+//  takesTwo(10,2);
+
+
+
+
+// *Conditionals*
+// Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+// function takesBooleanAndString(boolean, string) {
+//   if (boolean === true) {
+//     alert(string);
+//   } else if(boolean === false){
+//     console.log(string);
+//   }
+// }
+
+// takesBooleanAndString(true, "Hello");
+
+
+//*Loops*
+//Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+
+// function takesOne(num1) {
+//   for(let i=1; i<=num1; i++){
+//     if (i % 15 == 0) {
+//       console.log("FizzBuzz");
+//     } else if(i % 3 == 0) {
+//       console.log("fizz");
+//     } else if (i % 5 == 0) {
+//       console.log("buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+
+// takesOne(100);
+
+// ------------------ Arrays In it to win it -----------------
+
+//Create an array of movies with at least three movies.
+// let myMovies = ["Bring it on", "Bring It On Again", "Bring it on all or nothing"];
+
+
+//Using the array from above, store the first movie in a variable
+// let bringItOn = myMovies[0];
+// console.log(bringItOn);
+
+//Get the length of the original array and store it in a new variable
+// let myMoviesLenth = myMovies.length;
+// console.log(myMoviesLenth);
+
+//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+// let lastMovie = myMovies.slice(-1);
+// console.log(lastMovie);
+
+
+// ------------------ Team TreeHouse Array -----------------
+
+ 
+
