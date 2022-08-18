82c88dc2074173ce27894d701b4c1f8c40c8119b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-08-17 22:03:28 -0700

    Class 22 turbo Review

--- js/preview.js
+++ js/preview.js
@@ -711,3 +711,85 @@
 // }
 
 // console.log("You Finally enter a number less then or equal to 100");
+
+
+// ----------------------- Class 22 Turbo Review ----------------
+// *Variables*
+// Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
+
+// let favDrink = "Water";
+// console.log(favDrink);
+
+//Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
+
+// let randomWords = "mango apple";
+// if (randomWords.includes("apple")){
+//   console.log("yes is has apple");
+// }
+
+// *Functions*
+// Create a function that returns rock, paper, or scissors as randomly as possible
+// let choiceNumber = Math.ceil(Math.random() * 3 );
+
+// function rockPapperScissors() {
+//   let choiceNumber = Math.ceil(Math.random() * 3 );
+//     if (choiceNumber === 1) {
+//     return "rock";
+//     } else if (choiceNumber === 2){
+//       return "paper"
+//     } else {
+//       return "scissors";
+//     }
+// }
+
+// rockPapperScissors();
+// let computer  = rockPapperScissors();
+
+
+// *Conditionals*
+//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+
+// function didYouWin(player) {
+//   let computer  = rockPapperScissors();
+//   if( (player === "rock" && computer === "scissors") || (player === "papper" && computer === "rock") || (player === "scissors" && computer === "papper")){
+//     console.log("you win");
+//   } else if (player === computer  ) {
+//     console.log("its a tie");
+//   } else {
+//     console.log("you loose");
+//   }
+// }
+
+// didYouWin("rock");
+
+//*Loops*
+//Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+
+// function playGameXTimes(arr) {
+//   arr.forEach( choice => didYouWin(choice) );
+// }
+
+// playGameXTimes( ["rock", "papper", "scissors"]);
+
+// ----------------------- Class 22 Space Review ----------------
+
+//Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+// let numbers = [ 1, 2, 4, 5 ];
+// let sum = 0;
+
+// for(let i = 0; i < numbers.length; i++){
+//   sum += numbers[i];
+// }
+ 
+// alert(sum);
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+
+//Create a function that takes string
+//Print the reverse of that string to the console
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
